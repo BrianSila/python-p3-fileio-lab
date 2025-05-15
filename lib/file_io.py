bc75a2bd8075c0565f456a78f0b8d3d6a8c4b175
Brian <brian.sila1@student.moringaschool.com>
2025-05-15 21:06:08 +0300

    Fileio-lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
+    with open(f"{file_name}.txt", mode="a") as text_file:
+        text_file.write(append_content)
     pass
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode="r", encoding="utf-8") as text_file:
+        return text_file.read()
